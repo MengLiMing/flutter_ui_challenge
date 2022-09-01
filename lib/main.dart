934d1047f1586e3855db076873658d297d3d3977
Ming <13014795306@163.com>
2022-09-01 09:42:01 +0800

    android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_ui_challenge/examples/flutter_deer/deer_app.dart';
 
@@ -17,4 +20,10 @@ void main() {
       ),
     ),
   );
+  if (Platform.isAndroid) {
+    //透明沉浸式状态栏
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // transparent status bar
+    ));
+  }
 }
